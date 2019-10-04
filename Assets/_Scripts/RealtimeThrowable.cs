f4458ba46fbda581790e4924113f65e11f35bacd
Maf'j Alvarez <jay@rootinteractive.com>
2019-10-04 15:55:09 +0100

    added throwable component

---
+++ Assets/_Scripts/RealtimeThrowable.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine; 
+using Normal.Realtime;
+
+    public class RealtimeThrowable : MonoBehaviour
+    {
+
+        private RealtimeTransform bob;
+        private RealtimeView bill;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            bob = gameObject.GetComponent<RealtimeTransform>();
+            bill = gameObject.GetComponent<RealtimeView>();
+        }
+
+        public void Grabbed()
+        {
+            bob.RequestOwnership();
+            bill.RequestOwnership();
+        }
+
+    }
+
