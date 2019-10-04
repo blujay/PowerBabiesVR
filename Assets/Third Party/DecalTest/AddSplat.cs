4145a163199e16fc9df914a8d6c9efedc6ede174
Maf'j Alvarez <jay@rootinteractive.com>
2019-10-04 14:52:03 +0100

    added decals and renamed folder

--- Assets/Third Party/DecalTest/AddSplat.cs
+++ Assets/Third Party/DecalTest/AddSplat.cs
@@ -5,6 +5,8 @@ public class AddSplat : MonoBehaviour
 {
 
     public Transform decalPrefab;
+    public float MinScale = .2f;
+    public float MaxScale = 1f;
 
     // Start is called before the first frame update
     void Start()
@@ -24,6 +26,7 @@ public class AddSplat : MonoBehaviour
         var hit = other.contacts[0];
         var decalTransform = Instantiate(decalPrefab, hit.point, Quaternion.identity);
         decalTransform.localRotation = Quaternion.LookRotation(hit.normal);
+        decalTransform.localScale = Vector3.one * Random.Range(MinScale, MaxScale);
         var decal = decalTransform.GetComponent<Decal>();
         decal.SetData(decal.decalData);
     }
