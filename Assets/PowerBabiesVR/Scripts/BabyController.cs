f51d242f513a2a63d0e346de930000ab0d0ae537
Evan Griffiths <evangriffiths@hotmail.co.uk>
2019-10-05 21:07:28 +0100

    Re-applied realtime view to baby walker

--- Assets/PowerBabiesVR/Scripts/BabyController.cs
+++ Assets/PowerBabiesVR/Scripts/BabyController.cs
@@ -10,35 +10,35 @@ public class BabyController : MonoBehaviour
 
     Player Player;
 
-    void Awake()
-    {
-        Player = GetComponent<Player>();
-    }
+    //void Awake()
+    //{
+    //    Player = GetComponent<Player>();
+    //}
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        PointWalkerInLookDir();
-    }
+    //// Start is called before the first frame update
+    //void Start()
+    //{
+    //    PointWalkerInLookDir();
+    //}
 
-    // Update is called once per frame
-    void Update()
-    {
-        UpdateWalkerPos();
-    }
+    //// Update is called once per frame
+    //void Update()
+    //{
+    //    UpdateWalkerPos();
+    //}
 
-    void UpdateWalkerPos()
-    {
-        var walkerPos = Walker.transform.position;
-        walkerPos.x = Player.feetPositionGuess.x;
-        walkerPos.z = Player.feetPositionGuess.z;
-        Walker.transform.position = walkerPos;
-    }
+    //void UpdateWalkerPos()
+    //{
+    //    var walkerPos = Walker.transform.position;
+    //    walkerPos.x = Player.feetPositionGuess.x;
+    //    walkerPos.z = Player.feetPositionGuess.z;
+    //    Walker.transform.position = walkerPos;
+    //}
 
-    void PointWalkerInLookDir()
-    {
-        var walkerRot = Walker.transform.rotation.eulerAngles;
-        walkerRot.y = Cam.transform.rotation.eulerAngles.y;
-        Walker.transform.rotation = Quaternion.Euler(walkerRot);
-    }
+    //void PointWalkerInLookDir()
+    //{
+    //    var walkerRot = Walker.transform.rotation.eulerAngles;
+    //    walkerRot.y = Cam.transform.rotation.eulerAngles.y;
+    //    Walker.transform.rotation = Quaternion.Euler(walkerRot);
+    //}
 }
