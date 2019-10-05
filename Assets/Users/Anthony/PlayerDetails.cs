538b3541b6d8f2fa55b3d55fbb50e0e2719fbf14
Anthony Marmont <dev.anthonymarmont@gmail.com>
2019-10-05 16:03:16 +0100

    🚧 Started the player details stuff

---
+++ Assets/Users/Anthony/PlayerDetails.cs
@@ -0,0 +1,23 @@
+﻿using Normal.Realtime;
+using System;
+
+public class PlayerDetails : RealtimeComponent
+{
+	public string Identifier;
+
+	public PlayerDetailsSyncModel Details;
+
+	private void Start ()
+	{
+		var realtimeView = GetComponent<RealtimeView> ();
+
+		Identifier = Convert.ToBase64String (realtimeView.sceneViewUUID);
+
+		PlayerList.DiscoverPlayer (this);
+	}
+
+	private void OnDestroy ()
+	{
+		PlayerList.ForgetPlayer (this);
+	}
+}
---
+++ Assets/Users/Anthony/PlayerDetailsUI.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerDetailsUI : MonoBehaviour
+{
+	public PlayerDetails FollowOnStart;
+
+	[Space]
+	public Text Name;
+	public Text Score;
+
+	[Header ("Readonly")]
+	public PlayerDetails CurrentlyFollowing;
+
+	private void Start ()
+	{
+		if (FollowOnStart != null)
+		{
+			StartFollowing (FollowOnStart);
+		}
+	}
+
+	private void StartFollowing (PlayerDetails details)
+	{
+		if (CurrentlyFollowing.Details != null)
+		{
+			CurrentlyFollowing.Details.nameDidChange -= UpdateName;
+			CurrentlyFollowing.Details.scoreDidChange -= UpdateScore;
+		}
+
+		CurrentlyFollowing = details;
+
+		if (CurrentlyFollowing.Details != null)
+		{
+			UpdateName (CurrentlyFollowing.Details, CurrentlyFollowing.Details.name);
+			UpdateScore (CurrentlyFollowing.Details, CurrentlyFollowing.Details.score);
+
+			CurrentlyFollowing.Details.nameDidChange += UpdateName;
+			CurrentlyFollowing.Details.scoreDidChange += UpdateScore;
+		}
+		else
+		{
+			if (Name != null)
+			{
+				Name.text = "-";
+			}
+			if (Score != null)
+			{
+				Score.text = "0";
+			}
+		}
+	}
+
+	private void UpdateName (PlayerDetailsSyncModel model, string value)
+	{
+		if (Name != null)
+		{
+			Name.text = CurrentlyFollowing.Details.name;
+		}
+	}
+
+	private void UpdateScore (PlayerDetailsSyncModel model, int value)
+	{
+		if (Score != null)
+		{
+			Score.text = value.ToString ();
+		}
+	}
+}
