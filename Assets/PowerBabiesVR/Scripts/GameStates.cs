ef380d886f528098fd3c3f5df8790a184da69541
Paul Hayes <paul.hayes@entropedia.co.uk>
2019-10-05 15:57:19 +0100

    adding game state sync system. WIP

---
+++ Assets/PowerBabiesVR/Scripts/GameStates.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameState : RealtimeComponent
+{
+
+    public static GameState instance;
+
+    private GameStateModel _model;
+
+    public enum State
+    {
+        Loading,
+        Lobby,
+        Game
+    }
+
+    public State CurrentState {
+        protected set;
+        get;
+    }
+
+    void Awake()
+    {
+        _model._state = State.Lobby;
+        instance = this;
+    }
+
+    
+}
