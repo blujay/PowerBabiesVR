c322e4428953b12cd7089793e5d171dc557f73c6
Paul Hayes <paul.hayes@entropedia.co.uk>
2019-10-05 13:14:17 +0100

    Project structure changes

--- Assets/HitMeshWithDecal.cs
+++ Assets/HitMeshWithDecal.cs
@@ -4,13 +4,11 @@ using UnityEngine;
 
 public class HitMeshWithDecal : MonoBehaviour
 {
-    // Start is called before the first frame update
     void Start()
     {
         
     }
 
-    // Update is called once per frame
     void Update()
     {
         
