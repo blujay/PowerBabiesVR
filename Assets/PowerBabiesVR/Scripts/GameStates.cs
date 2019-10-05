b2ff185b8058744cb4ab72086f104f3de21b0649
Paul Hayes <paul.hayes@entropedia.co.uk>
2019-10-05 18:03:34 +0100

    Fixed gamestate model

--- Assets/PowerBabiesVR/Scripts/GameStates.cs
+++ Assets/PowerBabiesVR/Scripts/GameStates.cs
@@ -1,4 +1,5 @@
 ﻿using Normal.Realtime;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -24,9 +25,41 @@ public class GameStates : RealtimeComponent
 
     void Awake()
     {
-        _model._state = States.Lobby;
+        
         instance = this;
     }
 
-    
+    private GameStateModel model
+    {
+        set
+        {
+            if (_model != null)
+            {
+                // Unregister from events
+                _model.stateDidChange -= StateDidChange;
+            }
+
+            // Store the model
+            _model = value;
+
+            if (_model != null)
+            {
+                // Update the mesh render to match the new model
+                OnModelAttached();
+
+                // Register for events so we'll know if the color changes later
+                _model.stateDidChange += StateDidChange;
+            }
+        }
+    }
+
+    private void OnModelAttached()
+    {
+        _model.state = States.Lobby;
+    }
+
+    private void StateDidChange(GameStateModel model, States value)
+    {
+        
+    }
 }
