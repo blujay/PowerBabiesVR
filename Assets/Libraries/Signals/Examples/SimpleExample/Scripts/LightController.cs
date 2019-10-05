8b862f1ae6b97c8313f3fea8936a2e01bc9ba49e
Paul Hayes <paul.hayes@entropedia.co.uk>
2019-10-05 11:50:00 +0100

    Added singals lib

---
+++ Assets/Libraries/Signals/Examples/SimpleExample/Scripts/LightController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightController : MonoBehaviour {
+
+	[SerializeField]
+	Signal lightSignal;
+
+	[SerializeField]
+	Light targetLight;
+
+	void OnEnable()
+	{
+		lightSignal.Attach(OnLightSignal);
+	}
+
+	void OnDisable()
+	{
+		lightSignal.Detach(OnLightSignal);
+	}
+
+    private void OnLightSignal(object obj)
+    {
+        targetLight.enabled =!targetLight.enabled;
+    }
+}
