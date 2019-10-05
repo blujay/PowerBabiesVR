5bee14a244be0dfc0559989ac90589c963956e75
Paul Hayes <paul.hayes@entropedia.co.uk>
2019-10-05 20:45:52 +0100

    Created view elements differenciation

---
+++ Assets/PowerBabiesVR/Scripts/DifferenciateClientView.cs
@@ -0,0 +1,31 @@
+﻿using Normal.Realtime;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DifferenciateClientView : RealtimeComponent
+{
+    DifferenciateClientModel _model;
+
+    [SerializeField] GameObject[] localClientElements;
+    [SerializeField] GameObject[] remoteClientElements;
+
+    private DifferenciateClientModel model {
+        set {
+            _model = value;
+            UpdateElements( realtimeView.isOwnedLocally );
+        }
+    }
+
+    void UpdateElements(bool local) {
+        foreach (var element in localClientElements) {
+            element.SetActive(local);
+        }
+        foreach (var element in remoteClientElements)
+        {
+            element.SetActive(!local);
+        }
+
+
+    }
+}
