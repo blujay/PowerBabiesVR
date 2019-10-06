c625c52acb3ec5c826036744aa63d288d95132e8
Paul Hayes <paul.hayes@entropedia.co.uk>
2019-10-06 09:58:59 +0100

    Updated enviroment, set spawn position data

---
+++ Assets/PowerBabiesVR/Scripts/SpawnPositions.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnPositions : MonoBehaviour
+{
+    [SerializeField] Transform[] spawnPosition;
+
+    public SpawnPositions instance {
+        protected set;
+        get;
+    }
+
+    void Awake()
+    {
+        instance = this;
+    }
+
+    void Update()
+    {
+        
+    }
+}
