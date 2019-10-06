a114acb7b62687006efe293ff5158b0c8820a90f
Andy Baker <andy@andybak.net>
2019-10-06 15:32:54 +0100

    Splats are time limited and parented to the objects they hit

--- Assets/PowerBabiesVR/Scripts/AddSplat.cs
+++ Assets/PowerBabiesVR/Scripts/AddSplat.cs
@@ -7,10 +7,12 @@ public class AddSplat : MonoBehaviour
 
     public ScriptableDecal decalData;
     public float VelocityThreshold = 0.1f;
+    public float ElapsedTimeThreshold = 0.2f;
     public Color vfxColor = Color.red;
     public float MinScale = .2f;
     public float MaxScale = 1f;
 
+    private float LastDecalTime;
     private Rigidbody rb;
     private SplatParticles splatParticles;
 
@@ -19,7 +21,7 @@ public class AddSplat : MonoBehaviour
     {
         rb = gameObject.GetComponent<Rigidbody>();
         splatParticles = FindObjectOfType<SplatParticles>();
-
+        LastDecalTime = Time.time;
     }
 
     private void OnCollisionEnter(Collision other)
@@ -27,14 +29,15 @@ public class AddSplat : MonoBehaviour
         ContactPoint hit = other.contacts[0];
         float velocity = hit.thisCollider.attachedRigidbody.velocity.magnitude;
         if (velocity < VelocityThreshold) return;
-
+        if (Time.time - LastDecalTime < ElapsedTimeThreshold) return;
+        LastDecalTime = Time.time;
         Decal decal = DecalSystem.GetDecal(
             hit.point,
             Quaternion.LookRotation(hit.normal),
             Vector2.one * Random.Range(MinScale, MaxScale),
             decalData, true
         );
-
+        decal.gameObject.transform.parent = hit.otherCollider.transform;
         splatParticles.Launch(hit.point, vfxColor);
     }
 }
