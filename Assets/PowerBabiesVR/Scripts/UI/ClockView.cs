362ced942b1ea3126a7505b24980cb3618955993
Paul Hayes <paul.hayes@entropedia.co.uk>
2019-10-06 16:34:51 +0100

    Fixed bug in gmaestate change events :bug:

--- Assets/PowerBabiesVR/Scripts/UI/ClockView.cs
+++ Assets/PowerBabiesVR/Scripts/UI/ClockView.cs
@@ -4,7 +4,8 @@ using UnityEngine;
 
 public class ClockView : MonoBehaviour
 {
-    //Transform ClockHand;
+    [SerializeField] GameCountdown countdown;
+    [SerializeField] Transform clockHand;
 
     void Start()
     {
