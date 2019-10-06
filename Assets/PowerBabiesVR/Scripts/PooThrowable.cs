67a5c832461453cf9638e1fe310da03601acb401
Evan Griffiths <evangriffiths@hotmail.co.uk>
2019-10-06 13:03:39 +0100

    Poo now comes out the poo portal when in grab area and trigger pressed (yay?)

---
+++ Assets/PowerBabiesVR/Scripts/PooThrowable.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR.InteractionSystem;
+
+public class PooThrowable : Throwable
+{
+    public void AttachToHand(Hand hand)
+    {
+        transform.position = hand.transform.position;
+        OnAttachedToHand(hand);
+    }
+}
